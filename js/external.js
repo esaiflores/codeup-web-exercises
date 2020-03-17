ef075727e0cb5c3199722c69dca2ea99f15827f4
Esai Flores <flores.esaijonathan@gmail.com>
2020-03-17 09:36:59 -0500

    made new js files utilizing prompts and alerts, and fixed other pizza, warmmups, and css files

---
+++ js/external.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('Hello from external JS.');
+
+alert("Welcome to my website!");
+
+
+
+
+var color = prompt("What is your favorite color?");
+var response = "Great, " + color + " is my favorite color too!";
+alert(response);
