b9cd7d4c87e687a6b55a975fc008078e663a6bf9
Esai Flores <flores.esaijonathan@gmail.com>
2020-03-24 14:48:55 -0500

    performed ice cream loop

--- js/while.js
+++ js/while.js
@@ -5,3 +5,25 @@ while (i <= 65536){
     i = (i *2);
 
 }
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+Math.floor(Math.random() * 5) + 1;
+
+console.log(allCones);
+console.log(customerCones);
+
+do {
+    customerCones = Math.floor(Math.random() * 5) + 1;
+
+    if (customerCones > allCones) {
+        console.log("I cannot sell you " + customerCones + " because I only have " + allCones);
+    } else {
+        console.log('I can sell you  ' + customerCones + " because i have " + allCones);
+        allCones -= customerCones;
+    }
+
+
+} while (allCones >= 0);
+console.log('Yay! I sold them all ! ');
