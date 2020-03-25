93f40084463cf38a44d837f95c8738c18301d706
Esai Flores <flores.esaijonathan@gmail.com>
2020-03-25 10:45:42 -0500

    did odd or even problems, and the number problems including counting down

--- js/for_loops.js
+++ js/for_loops.js
@@ -6,3 +6,26 @@ function showMultiplicationTable (num) {
     }
 }
 showMultiplicationTable(7);
+
+var x;
+
+
+function isOddOrEven() {
+    for (i=1; i <= 10; i++){
+        var random = Math.floor(Math.random() * 180) + 20;
+        if(random % 2 === 0){
+            console.log(random + " is an even number");
+        }else{
+            console.log(random + " is an odd number");
+        }
+    }
+}
+isOddOrEven();
+
+for (var i = 1; i <= 9; i++) {
+    console.log(toString(i).repeat(i));
+}
+
+for (var d= 100; d >= 5; d-=5) {
+    console.log(d)
+}
